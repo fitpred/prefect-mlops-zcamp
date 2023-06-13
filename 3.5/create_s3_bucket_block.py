5664ebd3fd4c2263cb3dfe68588c294428eb6dd9
s.paliy <s.paliy@pin-up.business>
2023-06-13 08:58:54 +0300

    homework 3

--- 3.5/create_s3_bucket_block.py
+++ 3.5/create_s3_bucket_block.py
@@ -4,7 +4,7 @@ from prefect_aws import S3Bucket, AwsCredentials
 
 def create_aws_creds_block():
     my_aws_creds_obj = AwsCredentials(
-        aws_access_key_id="AKIAZJWGWT7ZENTY4QWV", aws_secret_access_key="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"
+        aws_access_key_id="AKIAZJWGWT7ZHJLYDJMV", aws_secret_access_key="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"
     )
     my_aws_creds_obj.save(name="my-aws-creds", overwrite=True)
 
@@ -12,12 +12,16 @@ def create_aws_creds_block():
 def create_s3_bucket_block():
     aws_creds = AwsCredentials.load("my-aws-creds")
     my_s3_bucket_obj = S3Bucket(
-        bucket_name="https://mlops-zcamp.s3.amazonaws.com/data/", credentials=aws_creds # s3://mlops-zcamp/data/
+        bucket_name="test_bucket", credentials=aws_creds # s3://mlops-zcamp/data/
     )
-    my_s3_bucket_obj.save(name="s3-bucket-example", overwrite=True)
+    my_s3_bucket_obj.save(name="s3-bucket-test", overwrite=True)
 
 
 if __name__ == "__main__":
     create_aws_creds_block()
     sleep(5)
     create_s3_bucket_block()
+
+
+# AKIAZJWGWT7ZENTY4QWV
+# CgO5qoV6FNcnnV95MYKVAvPCgIEwVk51SG4RbeZr
